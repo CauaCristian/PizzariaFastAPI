2c2191455dbd07dcf91eda3fcdf7d464bf1e50a1
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-07-04 15:01:50 -0300

    atualização .env

--- src/core/database/DatabaseConnection.py
+++ src/core/database/DatabaseConnection.py
@@ -14,5 +14,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 Base = declarative_base()
