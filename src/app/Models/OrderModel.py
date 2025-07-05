5ac0cce88826965d830243a6e2ee7c355255eb69
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-07-04 21:25:30 -0300

    criação das models

--- src/app/Models/OrderModel.py
+++ src/app/Models/OrderModel.py
@@ -1,14 +1,15 @@
 
-from sqlalchemy import Column, Integer, ForeignKey,String
-
+from sqlalchemy import Column, Integer, ForeignKey, String, Float
 
 from src.core.database.Database import Base
 class OrderModel(Base):
     __tablename__ = "order"
     id = Column("id",Integer, primary_key=True, autoincrement=True)
     status = Column("status",String, default="PENDING",nullable=False)
+    price = Column("price",Float,nullable=False,default=0)
     user_id = Column("user_id",Integer, ForeignKey("user.id"),nullable=False)
 
-    def __init__(self, status, user_id):
+    def __init__(self, status, user_id, price = 0):
         self.status = status
         self.user_id = user_id
+        self.price = price
