cc6fd72fa3346f99a731c3791f2cf84833a1c7e9
CauaCristian <cauainocencio14@hotmail.com>
2025-07-06 00:19:02 -0300

    correção e soluçao erro alembic

--- src/core/database/Database.py
+++ src/core/database/Database.py
@@ -1,17 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+from sqlalchemy.orm import sessionmaker, declarative_base
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
 
 db = create_engine(DATABASE_URL)
 
